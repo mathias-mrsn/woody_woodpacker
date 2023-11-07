29038ae9eee3b2c76f42c4c401135d79a66b37a5
Xavier Challe <xchalle@student.42.fr>
2023-11-07 15:46:50 +0100

    ready to merge

--- src/convert_headers.c
+++ src/convert_headers.c
@@ -164,7 +164,6 @@ ELF32_FORMAT *convert64_32(ELF64_FORMAT *src, const STORED_FILE *sf)
 }
 ELF64_FORMAT *convert32_64(const ELF32_FORMAT *src, const STORED_FILE *sf)
 {
-    return NULL;
     ELF64_FORMAT *dest;
     if (!(dest = calloc(1, sizeof(ELF64_FORMAT))))
         return NULL;
